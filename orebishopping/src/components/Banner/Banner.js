7b2b76cc7992541196cf74ff905d388e3919a7b8
Mohamed Azeem <azeemshanr@gmail.com>
2024-05-07 09:43:50 +0530

    third commit

--- orebishopping/src/components/Banner/Banner.js
+++ orebishopping/src/components/Banner/Banner.js
@@ -45,7 +45,7 @@ const CustomSlide = ({ Subtext, imgSrc, text, buttonLink, buttonText }) => (
         {Subtext}
       </p>
 
-      <Link to="/about">
+      <Link to="/">
         <button className="bg-primeColor text-white text-lg font-bodyFont w-[185px] h-[50px] hover:bg-black duration-300 font-bold">
           {buttonText}
         </button>
